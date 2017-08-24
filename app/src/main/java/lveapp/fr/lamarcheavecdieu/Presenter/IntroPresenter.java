f4f41852ae0eaa5be9a73ddca037dfcfb2c69ccf
evangile2vie <evangile2vie@gmail.com>
2017-08-24 17:10:37 +0200

    first commit

---
+++ app/src/main/java/lveapp/fr/lamarcheavecdieu/Presenter/IntroPresenter.java
@@ -0,0 +1,8 @@
+package lveapp.fr.lamarcheavecdieu.Presenter;
+
+/**
+ * Created by Maranatha on 24/08/2017.
+ */
+
+public class IntroPresenter {
+}
---
+++ app/src/main/java/lveapp/fr/lamarcheavecdieu/View/Interfaces/IHomeView.java
@@ -0,0 +1,8 @@
+package lveapp.fr.lamarcheavecdieu.View.Interfaces;
+
+/**
+ * Created by Maranatha on 24/08/2017.
+ */
+
+public interface IHomeView {
+}
