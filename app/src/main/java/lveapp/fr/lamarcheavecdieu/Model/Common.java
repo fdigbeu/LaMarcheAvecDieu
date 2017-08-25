d1c2bd66ea2a5477f428645f6cca7ea8fa91dce1
evangile2vie <evangile2vie@gmail.com>
2017-08-25 18:52:46 +0200

    Adding method to save and get data from share preferences

--- app/src/main/java/lveapp/fr/lamarcheavecdieu/Model/Common.java
+++ app/src/main/java/lveapp/fr/lamarcheavecdieu/Model/Common.java
@@ -1,8 +1,22 @@
 package lveapp.fr.lamarcheavecdieu.Model;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
 /**
  * Created by Maranatha on 25/08/2017.
  */
 
 public class Common {
+    public static void saveDataInSharePreferences(Context context, String keyData, String contentData){
+        SharedPreferences sharedPreferences = context.getSharedPreferences("SHARED_PREFERENCES", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(keyData, contentData);
+        editor.commit();
+    }
+
+    public static String getDataFromSharePreferences(Context context, String keyData){
+        SharedPreferences sharedPreferences = context.getSharedPreferences("SHARED_PREFERENCES", Context.MODE_PRIVATE);
+        return sharedPreferences.getString(keyData, "");
+    }
 }
---
+++ app/src/main/java/lveapp/fr/lamarcheavecdieu/Presenter/CommonPresenter.java
@@ -0,0 +1,8 @@
+package lveapp.fr.lamarcheavecdieu.Presenter;
+
+/**
+ * Created by Maranatha on 25/08/2017.
+ */
+
+public class CommonPresenter {
+}
