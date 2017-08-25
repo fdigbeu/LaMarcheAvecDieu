2b77b42f5bbac94900f4b551fe38aab7bc43d92f
evangile2vie <evangile2vie@gmail.com>
2017-08-25 14:25:43 +0200

    Calling method retrieveUserAction()

--- app/src/main/java/lveapp/fr/lamarcheavecdieu/View/Activities/HomeActivity.java
+++ app/src/main/java/lveapp/fr/lamarcheavecdieu/View/Activities/HomeActivity.java
@@ -4,7 +4,10 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 
 import lveapp.fr.lamarcheavecdieu.Presenter.HomePresenter;
@@ -13,7 +16,7 @@ import lveapp.fr.lamarcheavecdieu.View.Interfaces.HomeInterface.IHomeActivity;
 
 public class HomeActivity extends AppCompatActivity implements IHomeActivity {
 
-    private FloatingActionButton fabShare;
+    private FloatingActionButton fabMenuMore;
     private Toolbar toolbar;
     private HomePresenter homePresenter;
 
@@ -29,16 +32,31 @@ public class HomeActivity extends AppCompatActivity implements IHomeActivity {
     @Override
     public void findWidgetsById() {
         toolbar = (Toolbar) findViewById(R.id.toolbar);
-        fabShare = (FloatingActionButton) findViewById(R.id.fab);
+        fabMenuMore = (FloatingActionButton) findViewById(R.id.fab_menu_more);
     }
 
     @Override
     public void setWidgetsEvents() {
         setSupportActionBar(toolbar);
-        fabShare.setOnClickListener(new View.OnClickListener() {
+        fabMenuMore.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                homePresenter.retrieveUserAction(view);
+            }
+        });
+    }
 
+    @Override
+    public void displayMenuMoreItem(View view) {
+        PopupMenu popup = new PopupMenu(HomeActivity.this, view);
+        MenuInflater inflater = popup.getMenuInflater();
+        inflater.inflate(R.menu.popup_menu_more, popup.getMenu());
+        popup.show();
+        popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                homePresenter.retrieveUserAction(item);
+                return false;
             }
         });
     }
