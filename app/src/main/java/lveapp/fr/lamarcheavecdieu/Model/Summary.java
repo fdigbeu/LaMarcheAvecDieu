41783b8c0a805c86be34aff7644ad896fe8d9984
evangile2vie <evangile2vie@gmail.com>
2017-08-25 18:55:30 +0200

    Adding class to get summary json file

--- app/src/main/java/lveapp/fr/lamarcheavecdieu/Model/Summary.java
+++ app/src/main/java/lveapp/fr/lamarcheavecdieu/Model/Summary.java
@@ -4,5 +4,44 @@ package lveapp.fr.lamarcheavecdieu.Model;
  * Created by Maranatha on 25/08/2017.
  */
 
-public class Sommary {
+public class Summary {
+    private String title;
+    private String titleKeyCode;
+    private String titleNumber;
+
+    public Summary(String title, String titleKeyCode, String titleNumber) {
+        this.title = title;
+        this.titleKeyCode = titleKeyCode;
+        this.titleNumber = titleNumber;
+    }
+
+    public Summary(String title, String titleKeyCode, String titleNumber, String subtitleKeyCode) {
+        this.title = title;
+        this.titleKeyCode = titleKeyCode;
+        this.titleNumber = titleNumber;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTitleKeyCode() {
+        return titleKeyCode;
+    }
+
+    public void setTitleKeyCode(String titleKeyCode) {
+        this.titleKeyCode = titleKeyCode;
+    }
+
+    public String getTitleNumber() {
+        return titleNumber;
+    }
+
+    public void setTitleNumber(String titleNumber) {
+        this.titleNumber = titleNumber;
+    }
 }
