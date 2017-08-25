8fb1f3c63655a191d3cd97fd0a1566c88f9a3208
evangile2vie <evangile2vie@gmail.com>
2017-08-25 14:23:54 +0200

    Adding method : retrieveUserAction() to manage user action

---
+++ app/src/main/java/lveapp/fr/lamarcheavecdieu/Model/Sommary.java
@@ -0,0 +1,8 @@
+package lveapp.fr.lamarcheavecdieu.Model;
+
+/**
+ * Created by Maranatha on 25/08/2017.
+ */
+
+public class Sommary {
+}
--- app/src/main/java/lveapp/fr/lamarcheavecdieu/Presenter/HomePresenter.java
+++ app/src/main/java/lveapp/fr/lamarcheavecdieu/Presenter/HomePresenter.java
@@ -1,7 +1,10 @@
 package lveapp.fr.lamarcheavecdieu.Presenter;
 
 import android.content.Context;
+import android.view.MenuItem;
+import android.view.View;
 
+import lveapp.fr.lamarcheavecdieu.R;
 import lveapp.fr.lamarcheavecdieu.View.Interfaces.HomeInterface.IHomeActivity;
 
 /**
@@ -18,5 +21,39 @@ public class HomePresenter {
 
     public void loadHomeData(Context context){
         this.context = context;
+        iHomeActivity.findWidgetsById();
+        iHomeActivity.setWidgetsEvents();
+    }
+
+    /**
+     * Retrieve event of user action
+     * @param view
+     */
+    public void retrieveUserAction(View view){
+        switch (view.getId()){
+            case R.id.fab_menu_more:
+                iHomeActivity.displayMenuMoreItem(view);
+                break;
+        }
+    }
+
+    /**
+     * Retrieve event of user action
+     * @param item
+     */
+    public void retrieveUserAction(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.action_continuer_lecture:
+                break;
+
+            case R.id.action_partager_appli:
+                break;
+
+            case R.id.action_verifier_update:
+                break;
+
+            case R.id.action_modifier_parametre:
+                break;
+        }
     }
 }
